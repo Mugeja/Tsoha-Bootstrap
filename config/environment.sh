892e5375c9786a3098964cb952f46eedb3d85a26
Arttu Sinisalo <arttu.s.sinisalo@gmail.com>
2018-01-19 12:53:21 +0200

    Sovellus alustettu, versionhallinta otettu käyttöön

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="swarttu"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="Tsoha"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
