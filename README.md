a70cc1a7bedd233b4a511d7c230fe1302629331a
Arttu <arttu.s.sinisalo@gmail.com>
2018-01-26 14:42:27 +0200

    viikko 2 kohta 4

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ Yleisiä linkkejä:
 
 * Linkki sovellukseeni: http://swarttu.users.cs.helsinki.fi/Tsoha/
 * Linkki dokumentaatiooni: https://github.com/Mugeja/Tsoha-Bootstrap/blob/master/doc/Tsoha%20dokumentti%20viikko%201.pdf
-
+* typography - tekstin asettelu ja tyylittely: https://getbootstrap.com/docs/3.3/css/
+* tables - taulukot: https://getbootstrap.com/docs/3.3/css/
+* forms - lomakkeet: https://getbootstrap.com/docs/3.3/css/
+* buttons - painikkeet: https://getbootstrap.com/docs/3.3/css/
 ## Työn aihe
 
 Fuksipassi. Helppo tapa tehdä ja ylläpitää fuksipassia joka vuosi tuleville fukseille, ei tarvitse enää kantaa mukana
--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -3,12 +3,23 @@
   class HelloWorldController extends BaseController{
 
     public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+      // make-metodi renderÃ¶i app/views-kansiossa sijaitsevia tiedostoja
+   	  echo 'Tämä on etusivu';
     }
 
     public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+      // Testaa koodiasi tÃ¤Ã¤llÃ¤
+      View::make('helloworld.html');
     }
+    public static function tehtavat_list(){
+    View::make('suunnitelmat/game_list.html');
+  }
+
+  public static function tehtavat_show(){
+    View::make('suunnitelmat/game_show.html');
+  }
+
+  public static function login(){
+    View::make('suunnitelmat/login.html');
+  }
   }
