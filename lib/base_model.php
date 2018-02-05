b0fc26773947adc843617b2565d73d36df1c93f2
Arttu <arttu.s.sinisalo@gmail.com>
2018-02-05 16:36:07 +0200

    nelosviikko alku

--- lib/base_model.php
+++ lib/base_model.php
@@ -5,7 +5,8 @@
     protected $validators;
 
     public function __construct($attributes = null){
-      // Käydään assosiaatiolistan avaimet läpi
+        parent::construct($attributes);
+      $this->validators = array('validate_name', 'validate_kuvaus');
       foreach($attributes as $attribute => $value){
         // Jos avaimen niminen attribuutti on olemassa...
         if(property_exists($this, $attribute)){
@@ -24,6 +25,13 @@
       }
 
       return $errors;
+    }public function validitate_string_length($string, $length){
+        $errors = array();
+        if ($string == '' || $string == NULL){
+            $errors[] = 'Tyhjä vastaus';
+        }
+        if (strlen($string) < $length) {
+            $errors[] = 'Liian lyhyt vastaus';
+        }
     }
-
   }
