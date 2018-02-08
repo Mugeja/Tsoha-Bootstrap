e1eeb7a4eca9c447f385ddeabe172dc2201bbc00
Arttu Sinisalo <arttu.s.sinisalo@gmail.com>
2018-02-08 13:26:39 +0200

    väli commit

--- app/models/Tehtava.php
+++ app/models/Tehtava.php
@@ -10,22 +10,16 @@ class Tehtava extends BaseModel {
 
     public static function tulostaTehtavat() {
         $query = DB::connection()->prepare('SELECT * FROM Tehtävä');
-
         $query->execute();
-
         $rivit = $query->fetchAll();
-
         $tehtavat = array();
-
         foreach ($rivit as $rivi) {
-
             $tehtavat[] = new Tehtava(['id' => $rivi['id'],
                 'nimi' => $rivi['nimi'],
                 'hyväksyjä' => $rivi['hyväksyjä'],
                 'suoritettu' => $rivi['suoritettu'],
                 'kuvaus' => $rivi['kuvaus'],]);
         }
-
         return $tehtavat;
     }
 
@@ -33,7 +27,6 @@ class Tehtava extends BaseModel {
         $query = DB::connection()->prepare('SELECT * FROM Tehtävä WHERE id = :id LIMIT 1');
         $query->execute(array('id' => $id));
         $rivi = $query->fetch();
-
         if ($rivi) {
             $tehtava = new Tehtava(array(
                 'id' => $rivi['id'],
